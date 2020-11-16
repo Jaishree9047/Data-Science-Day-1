fd1124bafffc18c794eb7d570fbefb70cec9faeb
Jaishree9047 <74557317+Jaishree9047@users.noreply.github.com>
2020-11-16 22:14:34 +0530

    Day 1 Assignment submitted

---
+++ Day 1 Assignment (1).py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Question-1
+
+# Ans- B. RAINBOW
+
+
+# In[2]:
+
+
+#Question-2
+
+print("LETS UPGRADE")
+
+
+# In[3]:
+
+
+#Question-3
+
+cost_price=int(input("cost price:"))
+selling_price=int(input("selling price:"))
+if cost_price<selling_price:
+    print("PROFIT")
+elif cost_price>selling_price:
+    print("LOSS")
+else:
+    print("NEITHER")
+
+
+# In[4]:
+
+
+#question-4
+
+euro=int(input())
+rupees=euro*80
+print(rupees)
+
+
+# In[ ]:
+
+
+
+
